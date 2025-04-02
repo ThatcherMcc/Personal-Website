6836cd5e5eb6599a1df523ab5905b73fc2f1e7bb
YeetleBandeetle <thatchermcclure@gmail.com>
2025-04-01 20:35:26 -0500

    Fixed spacing in global and added better images

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <ParallaxProvider>
       <>
         {/* Background Image */}
-        <header className="w-full h-[80vh] bg-[url('/images/greenBG.jpg')] bg-contain bg-center bg-fixed flex flex-col justify-center items-center z-0">
+        <header className="w-full h-[80vh] bg-[url('/images/BlueBG2.jpg')] bg-cover bg-center bg-fixed flex flex-col justify-center items-center z-0">
           {/* Transparent Navigational Tabs */}
           <nav className="fixed top-0 right-0 w-full p-5 pr-7 z-50"> 
             <ul className="flex justify-end space-x-6 text-white text-vw-sm font-DM font-bold tracking-normal">
@@ -20,26 +20,28 @@ export default function Home() {
           </nav>
           
           {/* Bio and Photo  */}
-          <div className="flex w-full justify-around items-center">
+          <div className="flex w-full items-center justify-evenly gap-8">
             {/* Left Side - Name & Bio */}
             <Parallax speed={-10}>
-              <div className="text-white/90 max-w-vw-md bg-black/60">
-                <h1 className="text-vw-md font-bold">Thatcher McClure</h1>
-                <p className="mt-4 text-lg md:text-xl font-alfaSlab">
-                  I'm a software developer passionate about building creative digital experiences. Check out my projects below!
-                </p>
+              <div className="text-white max-w-vw-md">
+                <div>
+                  <h1 className="text-vw-md font-bold">Thatcher McClure</h1>
+                  <p className="mt-4 text-lg md:text-xl font-DM">
+                    I'm a software developer passionate about building creative digital experiences. Check out my projects below!
+                  </p>
+                </div>
               </div>
             </Parallax>
 
             {/* Right Side - Profile Image */}
             <div>
               <Parallax speed={-10}>
-                <Image 
-                  src="/images/lebronImage.jpg" 
+                <Image
+                  src="/images/TMHead.jpeg" 
                   alt="Thatcher McClure" 
-                  width={400} 
-                  height={400} 
-                  className="rounded-lg shadow-lg"
+                  width={350} 
+                  height={350} 
+                  className="rounded-full border-8 border-white"
                 />
               </Parallax>
             </div>
@@ -48,8 +50,8 @@ export default function Home() {
         
 
         {/* Skills Section */}
-        <div className="float-child flex flex-col min-h-screen items-center gap-4">
-          <div className="flex-container">
+        <div className="flex flex-col min-h-screen items-center gap-4">
+          <div className="">
             <h2 className="text-5xl font-semibold pt-14">Skills</h2>
           </div>
           <ProductCard />
